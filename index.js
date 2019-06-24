a4ac31fc5293aa6d08b65ea2b0a8f044ca4cb2ea
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-06-24 16:13:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+function batteries() {
+
 let totalBatteries = batterBatches.reduce(function(total, currentValue){
   
   return total + currentValue 
   
 })
+  
+}
